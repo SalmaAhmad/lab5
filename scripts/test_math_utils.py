8e64f2982483ab527b4417d5881551ac902b1e55
SalmaAhmad <salmasameer1000@gmail.com>
2025-11-02 20:18:17 +0200

    5

--- scripts/test_math_utils.py
+++ scripts/test_math_utils.py
@@ -1,16 +1,4 @@
 
-
-
-
-from maths_utils import add, subtract
-
-from .maths_utils import add, subtract
-
-from maths_utils import add, subtract
-
-import maths_utils
-from maths_utils import add, subtract
-
 from maths_utils import add, subtract
 
 def test_add():
